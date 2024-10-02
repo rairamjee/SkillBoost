a0cd480f2d8b2c8a016375c2623464e3ad1a4e20
ramjeerai <110187607+rairamjee@users.noreply.github.com>
2024-10-02 15:30:39 +0530

    Update page.tsx

--- frontend/src/app/dashboard/settings/page.tsx
+++ frontend/src/app/dashboard/settings/page.tsx
@@ -1,7 +1,21 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 function Settings() {
   return (
-    <div>Settings</div>
+    <Tabs defaultValue="trainings" className="w-[400px]">
+  <TabsList>
+    <TabsTrigger value="trainings">Trainings</TabsTrigger>
+    <TabsTrigger value="employee">Employees</TabsTrigger>
+  </TabsList>
+  <TabsContent value="trainings">
+    Some Basic settings Related to Trainings 
+  </TabsContent>
+  <TabsContent value="employee">
+    Some Basic settings Related to Employees
+  </TabsContent>
+</Tabs>
+
   )
 }
 
-export default Settings;
+export default Settings
